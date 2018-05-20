8b1d71dea788a6411fdca1c6214ef99a35a2d958
Per Jahn <perjahn@gmail.com>
2018-05-20 23:01:25 +0200

    2

---
+++ simplesource/Controllers/RootController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace simplesource.Controllers
+{
+    [Produces("application/json")]
+    [Route("")]
+    public class RootController : Controller
+    {
+        // POST /
+        [HttpPost]
+        public string Post([FromBody]string value)
+        {
+            using (StreamWriter sw = new StreamWriter("logR.txt", true))
+            {
+                sw.WriteLine($"{DateTime.UtcNow:HH:mm:ss}: root");
+            }
+
+            return "{ \"aa\": \"11\", \"bb\": \"rr\" }";
+        }
+    }
+}
---
+++ simplesource/Controllers/SearchController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace simplesource.Controllers
+{
+    [Produces("application/json")]
+    [Route("search")]
+    public class SearchController : Controller
+    {
+        // POST /search
+        [HttpPost]
+        public string Post([FromBody]string value)
+        {
+            using (StreamWriter sw = new StreamWriter("logS.txt", true))
+            {
+                sw.WriteLine($"{DateTime.UtcNow:HH:mm:ss}: search");
+            }
+
+            dynamic j = new JObject();
+            j.aa = 11;
+            j.bb = "ss";
+
+            return j.ToString();
+        }
+    }
+}
